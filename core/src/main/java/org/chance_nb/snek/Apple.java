29462020442055e5a3738d08a69ead20950e2065
chance-nb <96649398+chance-nb@users.noreply.github.com>
2025-03-04 13:14:36 +0100

    make apple movement more chaotic

--- core/src/main/java/org/chance_nb/snek/Apple.java
+++ core/src/main/java/org/chance_nb/snek/Apple.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Apple extends Consumable {
     private float lastUpdate = 0f;
+    private float nextMoveDirChange = 0.7f;
     private Vector2 moveDir = new Vector2();
 
     public Apple(Main main, float posx, float posy) {
@@ -13,10 +14,12 @@ public class Apple extends Consumable {
 
     @Override
     public void moreUpdates(GameScreen parent, float delta) {
-        if (main.state.movingAppleModifier) {
-            if (parent.time - lastUpdate > 0.7f) {
-                lastUpdate = parent.time;
-                moveDir.set((float) Math.random() * 4f - 2f, (float) Math.random() * 4f - 2f).nor();
+        if (main.state.movingAppleModifier) { // when apples should move
+            if (parent.time - lastUpdate > nextMoveDirChange) { // if we haven't changed direction in a bit
+                lastUpdate = parent.time; // reset timer
+                nextMoveDirChange = (float) Math.random() + 0.3f;
+                moveDir.set((float) Math.random() * 4f - 2f, (float) Math.random() * 4f - 2f); // set new random move
+                                                                                               // direction
             }
             this.pos.add(moveDir.cpy().scl(delta));
         }
@@ -24,9 +27,9 @@ public class Apple extends Consumable {
 
     @Override
     protected void onCollision(GameScreen parent, float delta) {
-            this.setPos(MathUtils.random(main.worldWidth), MathUtils.random(main.worldHeight));
-            parent.lastPiece = new TailPiece(main, parent.lastPiece.pos.cpy(), parent.lastPiece);
-            main.collect.play();
-            parent.points += 1;
+        this.setPos(MathUtils.random(main.worldWidth), MathUtils.random(main.worldHeight));
+        parent.lastPiece = new TailPiece(main, parent.lastPiece.pos.cpy(), parent.lastPiece);
+        main.collect.play();
+        parent.points += 1;
     }
 }
